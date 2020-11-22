1abb42c2eaa6dd13bae7dda505f342eaa5bf6eb9
Tim Paul <tmp2822d@vt.edu>
2020-11-22 09:26:51 -0500

    Food items can now generate three workouts

--- fooditems.cpp
+++ fooditems.cpp
@@ -1,27 +1,41 @@
 #include "fooditems.h"
 
-FoodItems::FoodItems()
+FoodItems::FoodItems() : EER(0), duration(15), caloriesConsumed(0)
 {
     foods = {
-        {"Apple", 72}, {"Banana", 105},
-        {"Slice of bread", 66}, {"Carrots raw 1 cup", 52},
-        {"Chili 1 cup", 287}, {"Oatmeal 1 cup", 147},
-        {"Spaghetti 1 cup", 313}, {"Backed potato", 161},
-        {"Pizza one slice", 298}, {"Rice 1 cup", 205},
-        {"Hot dog", 137}, {"Pork chop 8 ounce", 505},
-        {"Steak 8 ounce", 679}, {"Tuna 8 ounce", 250}};
+        {"Apples", 95}, {"Bananas", 105},
+        {"Beans", 42}, {"Beef", 338},
+        {"Bread", 79}, {"Broccoli", 50},
+        {"Carrots", 30}, {"Chicken", 335},
+        {"Pasta", 75}, {"Potatoes", 110},
+        {"Rice", 206}, {"Seafood", 280}};
 }
 
 int FoodItems::GetFoodCalories(QString food)
 {
     auto mapPos = foods.find(food);
+    return mapPos->second;
+}
 
-    if(mapPos == foods.end())
+QList<QString> FoodItems::GenerateFoods(QList<QString> preferedFoods, Profile prof)
+{
+    generatedFoods.clear();
+    availableFoods = preferedFoods;
+    PickFoods();
+    return generatedFoods;
+}
+
+void FoodItems::PickFoods()
+{
+    caloriesConsumed = 0;
+
+    for(int i = 0; i < availableFoods.size(); i++)
     {
-        return -1; // not a valid food item
-    }
-    else
-    {
-        return mapPos->second;
+        if(generatedFoods.size() < 3)
+        {
+            QString foodItem = availableFoods[i];
+            caloriesConsumed += GetFoodCalories(foodItem);
+            generatedFoods.push_front(foodItem);
+        }
     }
 }
--- fooditems.h
+++ fooditems.h
@@ -4,14 +4,24 @@
 #include <QObject>
 #include <map>
 
+#include "profile.h"
+
 class FoodItems
 {
 public:
     FoodItems();
     int GetFoodCalories(QString food);
+    QList<QString> GenerateFoods(QList<QString> preferedFoods, Profile prof);
 
 private:
+    void PickFoods();
     std::map<QString, int> foods;
+    QList<QString> generatedFoods;
+    QList<QString> availableFoods;
+    int EER;
+    int duration;
+    int caloriesConsumed;
+
 };
 
 #endif // FOODITEMS_H
