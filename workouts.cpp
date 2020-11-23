61428076604aec0156409443408601e79544793b
Raam <47303550+Raam0@users.noreply.github.com>
2020-11-23 18:30:59 -0500

    Add files via upload

--- workouts.cpp
+++ workouts.cpp
@@ -1,65 +1,164 @@
 #include "workouts.h"
 
-Workouts::Workouts() : goal(""), EER(0), duration(15), caloriesBurned(0)
+Workouts::Workouts()
 {
     //The integer value is the calories burned in 1 minute
     workoutData = {
         {"WeightLifting", 7}, {"Yoga", 5},
-        {"Hiking", 11}, {"Cycling", 10},
+        {"Hiking", 11}, {"Bicycing", 10},
         {"Running", 16}, {"Swimming", 12},
         {"Sports", 6}, {"Dancing", 7},
         {"Walking", 6}
     };
 }
 
-int Workouts::GetWorkoutCalories(QString workout)
+//Algorithm for generating 3 reccomended workouts
+void Workouts::GenerateWorkouts(int CurrentCals)
 {
-    auto mapPos = workoutData.find(workout);
+    //clear all previous reccomendations
+    generatedWorkouts.clear();
+    timeWorkouts.clear();
 
-    if(mapPos == workoutData.end())
+    //target calories to be burned through exercise
+    int target = CurrentCals - calories;
+
+    //If plan = 0, they chose to lose weight, target is 500 below the Goal
+    if(plan == 0)
     {
-        return -1; // not a valid workout
+        target -= 500;
+    }
+
+    //If they are already at less Calories than the target, reccomend no exercise (0 minutes for each exercise)
+    if(target <= 0)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            generatedWorkouts.push_back(availableWorkouts[index]);
+            timeWorkouts.push_back(0);
+            index++;
+            if(index == availableWorkouts.size())
+                index = 0;
+        }
     }
     else
     {
-        return mapPos->second;
-    }
-}
-
-int Workouts::CalcCaloriesBurned(int time, QString workout)
-{
-    int workoutCalories = GetWorkoutCalories(workout);
-    return time*workoutCalories;
-}
-
-//NOTE//
-//Will try to factor in time into calculation, will ask natalie add UI features for time and
-//physical activity level
-QList<QString> Workouts::GenerateWorkouts(QList<QString> preferedWorkouts)
-{
-    generatedWorkouts.clear(); // start new list
-    availableWorkouts = preferedWorkouts;
-    PickWorkouts();
-    return generatedWorkouts;       
-}
-
-//500 calories fewer
-void Workouts::PickWorkouts()
-{
-    caloriesBurned = 0;
-
-    for(int i = 0; i < availableWorkouts.size(); i++)
-    {
-        if(generatedWorkouts.size() < 3)
+        for(int i = 0; i < 3; i++)
         {
-            QString workout = availableWorkouts[i];
-            caloriesBurned += CalcCaloriesBurned(duration, workout);
-            generatedWorkouts.push_front(workout);
+            generatedWorkouts.push_back(availableWorkouts[index]);
+            timeWorkouts.push_back(target/workoutData[availableWorkouts[index]]);
+            index++;
+            if(index == availableWorkouts.size())
+                index = 0;
         }
     }
 }
 
-int Workouts::GetCaloriesBurned()
+void Workouts::saveData()
 {
-    return caloriesBurned;
+    //set up strings to be put in save file
+    std::string nL = "\n";
+    const char* newLine = nL.c_str();
+
+    std::string s = std::to_string(calories);
+    char const *cals = s.c_str();
+
+    std::string s1 = std::to_string(plan);
+    char const *currPlan = s1.c_str();
+
+    std::string s2 = std::to_string(index);
+    char const *currIndex = s2.c_str();
+
+    QFile file(QDir::current().absolutePath() + "/save.txt");
+    if(file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        file.write(cals);
+        file.write(newLine);
+
+        file.write(currPlan);
+        file.write(newLine);
+
+        file.write(currIndex);
+        file.write(newLine);
+
+        for (int i = 0; i < availableWorkouts.size(); ++i) {
+            file.write(availableWorkouts.at(i).toStdString().c_str());
+            file.write(newLine);
+        }
+    }
+    else
+        qDebug() << "Failed to save data";
 }
+
+int Workouts::loadData()
+{
+    int state = 0;
+    QFile file(QDir::current().absolutePath() + "/save.txt");
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "Failed to open";
+        availableWorkouts = { "temp"};
+        calories = 0;
+        plan = 0;
+        index = 0;
+        saveData();
+        return 1;
+    }
+    else
+    {
+        while (!file.atEnd()) {
+            QString line = file.readLine();
+            if(state == 0)
+            {
+                calories = line.toInt();
+                state = 1;
+            }
+            else if(state == 1)
+            {
+                plan = line.toInt();
+                state = 2;
+            }
+            else if(state == 2)
+            {
+                index = line.toInt();
+                state = 3;
+            }
+            else if(state == 3)
+            {
+                int pos = line.indexOf(QChar('\r'));
+                availableWorkouts.push_back(line.left(pos));
+            }
+        }
+    }
+    return 0;
+}
+
+QList<QString> Workouts::getReccomendations()
+{
+    return generatedWorkouts;
+}
+
+QList<int> Workouts::getTimeReccomendations()
+{
+    return timeWorkouts;
+}
+
+void Workouts::setAvailableWorkouts(QList<QString> q1)
+{
+    availableWorkouts = q1;
+}
+
+void Workouts::setCalories(int c1)
+{
+    calories = c1;
+}
+
+void Workouts::setPlan(int p1)
+{
+    plan = p1;
+}
+
+void Workouts::setIndex(int i1)
+{
+    index = i1;
+}
+
+
--- workouts.h
+++ workouts.h
@@ -3,28 +3,44 @@
 
 #include <QObject>
 #include <map>
-
+#include <iostream>
+#include <ctime>
+#include <QFile>
+#include <QDir>
+#include <string>
+#include <QDebug>
 #include "profile.h"
 
 class Workouts
 {
 public:
     Workouts();
-
-    int CalcCaloriesBurned(int time, QString workout);//time in minutes
-    int GetWorkoutCalories(QString workout);
-    QList<QString> GenerateWorkouts(QList<QString> preferedWorkouts);
-    int GetCaloriesBurned();
+    void GenerateWorkouts(int CurrentCals);
+    void saveData();
+    int loadData();
+    QList<QString> getReccomendations();
+    QList<int> getTimeReccomendations();
+    void setAvailableWorkouts(QList<QString> q1);
+    void setCalories(int c1);
+    void setPlan(int p1);
+    void setIndex(int i1);
 
 private:
-    void PickWorkouts();
     std::map<QString, int>  workoutData;
     QString goal;
-    int EER;
-    int duration;
-    int caloriesBurned;
+    //generatedWorkouts is 3 Workouts to Reccomend
     QList<QString> generatedWorkouts;
+
+    //Timeworkouts is time to spend on each workout
+    QList<int> timeWorkouts;
+
+    //Available workouts is the list of their preferred workouts
     QList<QString> availableWorkouts;
+
+    //calories is their needed cals per day, plan is lose or maintain weight, and index is where to start reccs
+    int calories;
+    int plan;
+    int index;
 };
 
 #endif // WORKOUTS_H
